289c7677ab907a87b6d33da187ca9552ab5fd444
Faisal Hussain <me@imfaisal.pro>
2026-01-13 00:28:33 +0500

    Local Install Issue Fix - Rev 6

---
+++ react-native.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dependency: {
+    platforms: {
+      android: {
+        packageImportPath:
+          'import com.realtimeaudio.RealtimeAudioAnalyzerPackage;',
+        packageInstance: 'new RealtimeAudioAnalyzerPackage()',
+      },
+    },
+  },
+};
